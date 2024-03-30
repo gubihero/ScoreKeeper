423854f076be7176c3c382621fd9a5059d8cf43b
James Gubler <gubimail@gmail.com>
2024-03-30 15:18:07 -0600

    adds components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,26 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
+        path: '/',
+        loadComponent: () => import('./components/landing/landing.component')
+            .then(m => m.LandingComponent),
+        title: 'Home'
+    },
+    {
         path: 'players',
         loadComponent: () => import('./components/player-list/player-list.component')
-            .then(m => m.PlayerListComponent)
+            .then(m => m.PlayerListComponent),
+        title: 'Players'
+    },
+    {
+        path: 'players/:id',
+        loadComponent: () => import('./components/player-detail/player-detail.component')
+            .then(m => m.PlayerDetailComponent)
+    },
+    {
+        path: 'games',
+        loadComponent: () => import('./components/game-list/game-list.component')
+            .then(m => m.GameListComponent),
+        title: 'Games'
     }
 ];
---
+++ src/app/components/select-game/select-game.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select-game',
+  standalone: true,
+  imports: [],
+  templateUrl: './select-game.component.html',
+  styleUrl: './select-game.component.scss'
+})
+export class SelectGameComponent {
+
+}
